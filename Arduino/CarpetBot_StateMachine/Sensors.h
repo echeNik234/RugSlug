d2eff72ae09c635526ae17297782badba950db57
echeNik234 <132436319+echeNik234@users.noreply.github.com>
2023-06-20 02:00:21 -0700

    Update Sensors.h

--- Arduino/CarpetBot_StateMachine/Sensors.h
+++ Arduino/CarpetBot_StateMachine/Sensors.h
@@ -1,26 +1,30 @@
-#ifndef SENSORS_H
+ #ifndef SENSORS_H
 #define SENSORS_H
 
 #include <Arduino.h>
 
-  #define IR1 48 // front right sensor
-  #define IR2 49 // front left sensor
-  #define IR3 50 // back right sensor 
-  #define IR4 51 // back left sensor
-  #define tiltSensor 52 // tilt sensor
-  #define cleanWater 53 // water level sensor for clean tank 
-  #define dirtyWater 54 // water level sensor for dirty tank
+  #define IR1 22 // front right sensor
+  #define IR2 23 // front left sensor
+  #define tiltSensor 40 // tilt sensor
+  #define cleanWater 41 // water level sensor for clean tank 
+  #define dirtyWater 42 // water level sensor for dirty tank
+  #define farBeaconSensor 39 // beacon detector for distance 
+  #define closeBeaconSensor A1 // beacon detector for up close
 
   int objectDetectedRight();
 
-  int objectDetectedLeft();
+  int objectDetectedLeft(); 
 
   int objectDetectedFront();
 
-  int objectDetectedBack(); 
+  // int objectDetectedBack(); 
 
   int waterLevel(int waterSensor);
 
   int robotFallenOver();
+
+  int farBeaconDetected();
+
+  int closeBeaconDetected();
 
 #endif
