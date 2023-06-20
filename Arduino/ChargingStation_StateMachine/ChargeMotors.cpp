65f79847e73aa90b456dcd8d48844899642735a8
echeNik234 <132436319+echeNik234@users.noreply.github.com>
2023-06-20 02:01:01 -0700

    Update ChargeMotors.cpp

--- Arduino/ChargingStation_StateMachine/ChargeMotors.cpp
+++ Arduino/ChargingStation_StateMachine/ChargeMotors.cpp
@@ -2,23 +2,15 @@
 #include <Servo.h>
 #include <Arduino.h>
 
-Servo myservo; // create servo object to control a servo
-int chargeServPos; // variable to store the servo position
+extern Servo myservo; // create servo object to control a servo
+int chargeServPos = 0; // variable to store the servo position
 
-void waterRelease(){
-  if chargeServPos != 0){
-    for  chargeServPos = 180; chargeServPos >= 0; chargeServPos -= 1) { // goes from 90 degrees to 0 degrees
-      // in steps of 1 degree
-      myservo.write chargeServPos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
-  }
+void waterOpen(){
+  Serial.print("Water Open");
+  myservo.write(90);
+}
 
-  else{
-    for  chargeServPos = 0; chargeServPos <= 180; chargeServPos += 1) { // goes from 0 degrees to 90 degrees
-      // in steps of 1 degree
-      myservo.write chargeServPos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
-  }
+void waterClose(){
+  Serial.print("Water Close");
+  myservo.write(0);
 }
