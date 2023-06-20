92da68f323675c3d23647bd9450df186064a61f4
echeNik234 <132436319+echeNik234@users.noreply.github.com>
2023-06-20 02:06:17 -0700

    Update ChargeMotors.cpp

--- Arduino/ChargingStation_StateMachine/ChargeMotors.cpp
+++ Arduino/ChargingStation_StateMachine/ChargeMotors.cpp
@@ -7,10 +7,10 @@ int chargeServPos = 0; // variable to store the servo position
 
 void waterOpen(){
   Serial.print("Water Open");
-  myservo.write(90);
+  myservo.write(-90);
 }
 
 void waterClose(){
   Serial.print("Water Close");
-  myservo.write(0);
+  myservo.write(90);
 }
