22ad47d7464aad39ada3c8be353a6e9ce6965150
echeNik234 <132436319+echeNik234@users.noreply.github.com>
2023-06-20 02:06:44 -0700

    Update ChargeSensors.cpp

--- Arduino/ChargingStation_StateMachine/ChargeSensors.cpp
+++ Arduino/ChargingStation_StateMachine/ChargeSensors.cpp
@@ -3,60 +3,62 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <ezButton.h>
-  
-  int Liquid_level = 0;
-  int state;
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire twoWire(ONE_WIRE_BUS);
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensor1(&twoWire);
+int Liquid_level = 0;
+int state;
 
+// ####################### PIN ASSINGMENT ######################
+OneWire twoWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+DallasTemperature sensor1(&twoWire); // Pass our oneWire reference to Dallas Temperature.
 ezButton limitSwitch1(Lswitch);  // create ezButton object that attach to pin 7;
 
-  float getTemp(){
-    float temperature;
-    sensor1.requestTemperatures();
-    temperature = sensor1.getTempFByIndex(0);
-    return temperature;
-  }
+// ####################### GET TEMP ######################
+float getTemp() {
+  float temperature;
+  sensor1.requestTemperatures();
+  temperature = sensor1.getTempFByIndex(0);
+  return temperature;
+}
 
-
-  int bumperTriggered(){
-    limitSwitch1.loop(); // MUST call the loop() function first
-
-  // if(limitSwitch1.isPressed())
-  //   Serial.println("The limit switch: UNTOUCHED -> TOUCHED");
-
-  // if(limitSwitch1.isReleased())
-  //   Serial.println("The limit switch: TOUCHED -> UNTOUCHED");
+// ####################### LIMIT SWITCH ######################
+int bumperTriggered() {
+  limitSwitch1.loop();  // MUST call the loop() function first
 
   state = limitSwitch1.getState();
-  if(state == HIGH)
-    // Serial.println("The limit switch: UNTOUCHED");
+  if (state == HIGH)
+    // Serial.println("The limit switch: UNTOUCHED"); // FIX ME: Only for testing purposes
     return 0;
   else
-    // Serial.println("The limit switch: TOUCHED");
+    // Serial.println("The limit switch: TOUCHED"); // FIX ME: Only for testing purposes
     return 1;
+}
+
+// ####################### WATER LEVEL SENSOR ######################
+int waterLevel() {
+  Liquid_level = digitalRead(waterTank);
+  if (Liquid_level == 1) {
+    return 1;
+  } else {
+    return 0;
   }
+}
 
+// ####################### HEATING ON######################
+void heatOn() {
+  digitalWrite(waterHeater, HIGH);
+}
 
-  int waterLevel(){
-    Liquid_level = digitalRead(waterTank);
-    if(Liquid_level == 1){
-      return 1;
-    }
-    else{
-      return 0;
-    }
-  }
+// ####################### HEATING OFF ######################
+void heatOff() {
+  digitalWrite(waterHeater, LOW);
+}
 
+// ####################### BEACON CS ON######################
+void beaconCSOn() {
+  digitalWrite(beaconCS, HIGH);
+}
 
-  void heatOn(){
-    digitalWrite(waterHeater, HIGH);
-  }
-
-
-  void heatOff(){
-    digitalWrite(waterHeater, LOW);
-  }
+// ####################### BEACON CS OFF ######################
+void beaconCSOff() {
+  digitalWrite(beaconCS, LOW);
+}
