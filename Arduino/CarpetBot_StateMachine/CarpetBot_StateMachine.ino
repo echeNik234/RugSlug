611b76650f918b47fbb2f05872ec9018108ce4f7
echeNik234 <132436319+echeNik234@users.noreply.github.com>
2023-12-15 15:03:51 -0800

    Update CarpetBot_StateMachine.ino
    Updated code for final submisison

--- Arduino/CarpetBot_StateMachine/CarpetBot_StateMachine.ino
+++ Arduino/CarpetBot_StateMachine/CarpetBot_StateMachine.ino
@@ -4,8 +4,10 @@
 
 
 //global defines
-
-
+int beacClose;
+int beacFar;
+double voltage;
+int tilt;
 
 //global variables
   //timer 1
@@ -16,19 +18,22 @@ const long period1 = 3000;
 //timer 2
 unsigned long prevTime2 = 0;
 unsigned long curTime2;
-const int forwardDuration = 4000;
+unsigned long forwardDuration = 4000;
 
 //timer 3
 unsigned long prevTime3 = 0;
 unsigned long curTime3;
-const int turnDuration = 7000;
+unsigned long turnDuration = 7000;
 
 int flag = 0;
-int numCycles = 8;
+int numCycles = 1;
 int cycleCount = 0;
 bool turning = false;
-
-
+unsigned long startTime;
+int cleaningState;
+int repeatCount = 2;
+int totalCount = 2;
+int testFlag = 1;
 
 
 
@@ -40,12 +45,13 @@ typedef enum
   CHARGING, //robot charges its battery at charging station
   FLUIDFILL, //water cleaning solution is put into the robots cleaning tank
   BEACONSEARCH, //robot begins searching for then navigating to spot beacon 
+  BEACONGRAB, //robot picks up the beacon
   CLEANING, //robot begins cleanig stain
   CHARGINGSEARCH, //robot begins searching for then navigating to charging statio beacon
   PAUSE //robot functions are paused from app or when bot is tipped over
 } roboStates; 
 
-roboStates curState = BEACONSEARCH;
+roboStates curState = CHARGING;
 roboStates prevState = CHARGING;
 
 //function that sets up the output and input pins to be called in void setup()
@@ -73,7 +79,7 @@ void pinSetup(){
   pinMode(cleanWater, INPUT); // water level sensor for dirty tank
   pinMode(farBeaconSensor, INPUT); // beacon detector for detecting from a distance
   pinMode(closeBeaconSensor, INPUT); // beacon detector for detecting up close
-  prevTime2 = millis();
+  // prevTime2 = millis();
 
 //arm pins
   pinMode(arm1, OUTPUT);
@@ -95,6 +101,8 @@ void pinSetup(){
 void setup() {
   // put your setup code here, to run once:
   pinSetup();
+  Serial.println("1. yes");
+  Serial.println("2. no");
   Serial.begin(115200);
 }
 
@@ -111,8 +119,24 @@ void runRoboSwitchCase()
   {
     //////////////////////////////////////////
     case CHARGING: {
-      if("app signal detected"){
+      Serial.println("Would you like to initiate cleaning? Enter 1 for yes or 2 for no:");
+      while (Serial.available() == 0){      
+      }
+      int choice = Serial.parseInt();
+
+      switch (choice){
+        case 1:
+          Serial.print("Entering FLUIDFILL state\n");
           curState = FLUIDFILL;
+        break;
+
+        case 2:
+          Serial.println("Staying in charging state");
+          curState = CHARGING;
+        break;
+
+        default:
+          Serial.println("Not a valid answer please try again");
       }
     break;
     }
@@ -121,9 +145,9 @@ void runRoboSwitchCase()
 
     //////////////////////////////////////////
     case FLUIDFILL:{
-      if (waterLevel(cleanWater) == 1 && waterLevel(dirtyWater) == 0){
-          curState = BEACONSEARCH;
-      }
+      delay(5000);
+      Serial.print("Entering BEACONSEARCH state\n");
+      curState = BEACONSEARCH;
     break;
     }
     //////////////////////////////////////////
@@ -131,11 +155,19 @@ void runRoboSwitchCase()
 
     //////////////////////////////////////////
     case BEACONSEARCH:{
-      if(robotFallenOver() == 1){
+      if(robotFallenOver() == 0){
+        Serial.print("Robot fallen over\n");
+        Serial.print("Entering PAUSE state\n");
         prevState = BEACONSEARCH;
         curState = PAUSE;
       }
 
+      // if(batteryCharge() <= 13.00){
+      //   Serial.print("Battery low");
+      //   Serial.print('\n');
+      //   curState = CHARGINGSEARCH;
+      // }
+      
       if(flag == 0){
         reverseDrive();
       }
@@ -146,10 +178,11 @@ void runRoboSwitchCase()
       }
 
       if(flag == 1){ //reset at end of entire process
-        if(farBeaconDetected() == 1){
+        if(farBeaconDetected() == 0){
           forwardDrive();
                 
           if(objectDetectedLeft() == 1 && objectDetectedRight() == 0){
+            Serial.print("Object on the left\n");
             tankRight();
             delay(1250);
             forwardDrive();
@@ -157,6 +190,7 @@ void runRoboSwitchCase()
           }
 
           if(objectDetectedRight() == 1 && objectDetectedLeft() == 0){
+            Serial.print("Object on the right\n");
             tankLeft();
             delay(1250);
             forwardDrive();
@@ -164,35 +198,22 @@ void runRoboSwitchCase()
           }
 
           if(objectDetectedFront() == 1){
+            Serial.print("Object in front\n");
             tankRight();
             delay(1250);
             forwardDrive();
             delay(1000);
           }
         }
-        if (farBeaconDetected() == 0){
+        if (farBeaconDetected() == 1){
           rotate();
         }
       }
 
 
-      if((closeBeaconDetected() <= 0) && (closeBeaconDetected() >= -3)){
-        stop();
-        delay(1000);
-        clawOpen();
-        delay(1000);
-        beaconLower();
-        delay(7000);
-        clawStop();
-        forwardDrive();
-        delay(500);
-        stop();
-        clawClose();
-        delay(1000);
-        beaconLift();
-        delay(8000);
-        clawStop();
-        curState = CLEANING;
+      if(closeBeaconDetected() == 1){
+        Serial.print("Entering BEACONGRAB state\n");
+        curState = BEACONGRAB;
       }
       break;
     }
@@ -200,46 +221,94 @@ void runRoboSwitchCase()
 
 
     //////////////////////////////////////////
+    case BEACONGRAB:{
+      if(robotFallenOver() == 0){
+        Serial.print("Robot fallen over\n");
+        Serial.print("Entering PAUSE state\n");
+        prevState = BEACONSEARCH;
+        curState = PAUSE;
+      }
+
+      if(batteryCharge() <= 13.0){
+        Serial.print("Battery low");
+        Serial.print('\n');
+        curState = CHARGINGSEARCH;
+      }
+      stop();
+      delay(1000);
+      clawOpen();
+      delay(1000);
+      beaconLower();
+      delay(3000);
+      clawStop();
+      forwardDrive();
+      delay(500);
+      stop();
+      clawClose();
+      delay(1000);
+      beaconLift();
+      delay(3000);
+      clawStop();
+      Serial.print("Entering CLEANING state\n");
+      Serial.print("\n");
+      curState = CLEANING;
+      break;
+    }
+    //////////////////////////////////////////
+
+
+    //////////////////////////////////////////
     case CLEANING:{
-      if(robotFallenOver() == 1){
+      unsigned long currentTime = millis();
+
+      if(robotFallenOver() == 0){
+        Serial.print("Robot fallen over\n");
+        Serial.print("Entering PAUSE state\n");
         prevState = CLEANING;
         curState = PAUSE;
       }
 
-      brushOn();
-      openWater();
-      //turn fan on
+      // if(batteryCharge() <= 13.00){
+      //   // Serial.print("Battery low");
+      //   // Serial.print('\n');
+      //   curState = CHARGINGSEARCH;
+      // }
 
-      if(cycleCount != numCycles){
-
-        if(turning == false){
-          forwardScrub();
+      if(cleaningState == 0){
+        brushOn();
+        openWater();
+        fanOn();
+        forwardScrub();
+        startTime = currentTime;
+        cleaningState = 1;
+      }
+      else if(cleaningState == 1 && currentTime - startTime >= forwardDuration){
+        stop();
+        startTime = currentTime;
+        cleaningState = 2;
+      }
+      else if(cleaningState == 2){
+        rotate();
+        startTime = currentTime;
+        cleaningState = 3;
+      }
+      else if(cleaningState == 3 && currentTime - startTime >= turnDuration){
+        stop();
+        cleaningState = 0;
+        repeatCount--;
+        Serial.print(repeatCount);
+        Serial.print("\n");
+        if(repeatCount > 0){
+          cleaningState = 0;
         }
-
-        if(curTime2 - prevTime2 >= forwardDuration){
-          stop();
-          turning = true;
+        else{
+          repeatCount = totalCount;
+          brushOff();
+          closeWater();
+          fanOff();
+          Serial.print("Entering CHARGINGSEARCH state\n");
+          curState = CHARGINGSEARCH;
         }
-
-        if(turning == true){
-          rotate();
-        }
-
-        if(curTime3 - prevTime3 >= turnDuration){
-          prevTime3 = curTime3;
-          prevTime2 = curTime2;
-          stop();
-          cycleCount += 1;
-          turning = false;
-        }
-      }     
-  
-      if(cycleCount == numCycles){
-        cycleCount = 0;
-        brushOff();
-        closeWater();
-        //turn off fan
-        curState = CHARGINGSEARCH; 
       }
       break;
     }
@@ -248,33 +317,50 @@ void runRoboSwitchCase()
 
     //////////////////////////////////////////
     case CHARGINGSEARCH:{
-      if(farBeaconDetected() == 1){
+      if(robotFallenOver() == 0){
+        Serial.print("Robot fallen over\n");
+        Serial.print("Entering PAUSE state\n");
+        prevState = BEACONSEARCH;
+        curState = PAUSE;
+      }
 
+      if(farBeaconDetected() == 0){
         forwardDrive();
               
         if(objectDetectedLeft() == 1 && objectDetectedRight() == 0){
+          Serial.print("Object on the left\n");
           tankRight();
           delay(1250);
           forwardDrive();
-          delay(1000);
+          delay(500);
         }
 
         if(objectDetectedRight() == 1 && objectDetectedLeft() == 0){
+          Serial.print("Object on the right\n");
           tankLeft();
           delay(1250);
           forwardDrive();
-          delay(1000);
+          delay(500);
         }
 
         if(objectDetectedFront() == 1){
+          Serial.print("Object in front\n");
           tankRight();
           delay(1250);
           forwardDrive();
-          delay(1000);
+          delay(500);
         }
       }
-      if (farBeaconDetected() == 0){
+      if (farBeaconDetected() == 1){
         rotate();
+      }
+
+      if(closeBeaconDetected() == 1){
+        flag = 0;
+        prevTime1 = curTime1;
+        stop();
+        Serial.print("Entering CHARGING state\n");
+        curState = CHARGING;
       }
       break;
     }
@@ -283,13 +369,15 @@ void runRoboSwitchCase()
 
     //////////////////////////////////////////
     case PAUSE:{
-      if(robotFallenOver() == 0){
+
+      stop();
+      clawStop();
+      brushOff();
+      fanOff();
+      if(robotFallenOver() == 1){
+        Serial.print("Entering previous state\n");
         curState = prevState;
       }
-
-      // if("Signal from app to resume cleaning"){
-      //   curState = prevState;
-      // }
       break;
     }
     //////////////////////////////////////////
@@ -302,69 +390,11 @@ void loop() {
   curTime2 = millis();
   curTime3 = millis();
 
+  
+//////////////////////////////////////////////////////////
   runRoboSwitchCase();
-
-  // if(robotFallenOver() == 1){
-  //       prevState = BEACONSEARCH;
-  //       curState = PAUSE;
-  //     }
-
-  //     if(flag == 0){
-  //       reverseDrive();
-  //     }
-
-  //     if(curTime1 - prevTime1 >= period1){ //set prevTime equal to curTime at end of entire process
-  //       flag = 1;
-  //       stop();
-  //     }
-
-  //     if(flag == 1){ //reset at end of entire process
-  //       if(farBeaconDetected() == 1){
-  //         forwardDrive();
-                
-  //         if(objectDetectedLeft() == 1 && objectDetectedRight() == 0){
-  //           tankRight();
-  //           delay(1250);
-  //           forwardDrive();
-  //           delay(1000);
-  //         }
-
-  //         if(objectDetectedRight() == 1 && objectDetectedLeft() == 0){
-  //           tankLeft();
-  //           delay(1250);
-  //           forwardDrive();
-  //           delay(1000);
-  //         }
-
-  //         if(objectDetectedFront() == 1){
-  //           tankRight();
-  //           delay(1250);
-  //           forwardDrive();
-  //           delay(1000);
-  //         }
-  //       }
-  //       if (farBeaconDetected() == 0){
-  //         rotate();
-  //       }
-  //     }
-
-
-  //     if((closeBeaconDetected() <= 0) && (closeBeaconDetected() >= -3)){
-  //       stop();
-  //       delay(1000);
-  //       clawOpen();
-  //       delay(1000);
-  //       beaconLower();
-  //       delay(7000);
-  //       clawStop();
-  //       forwardDrive();
-  //       delay(500);
-  //       stop();
-  //       clawClose();
-  //       delay(1000);
-  //       beaconLift();
-  //       delay(8000);
-  //       clawStop();
-  //       curState = CLEANING;
-  //     }
+//////////////////////////////////////////////////////////
+    //  voltage = batteryCharge();
+    //  Serial.print(voltage);
+    //  Serial.print("\n");
 }
